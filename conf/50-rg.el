bbd877a0c6147a6930be8a5281758646146a02ad
Flo Roeske <floroeske@protonmail.ch>
2022-06-16 10:48:33 +1200

    Set rg aliases

--- conf/50-rg.el
+++ conf/50-rg.el
@@ -2,3 +2,11 @@
   :ensure t)
 
 (rg-enable-default-bindings)
+
+(setq rg-custom-type-aliases
+  '(("cpp" .    "*.cpp *.cxx *.cc *.hpp *.h")
+    ("c" .      "*.c *.h")
+    ("py" .     "*.py")
+    ("org" .    "*.org")
+    ("log" .    "*.txt *.log")
+    ))
