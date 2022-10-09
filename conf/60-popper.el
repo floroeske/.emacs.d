9bc95cc2414f42cbf63f75d59c7e00a794383436
Flo Roeske <floroeske@protonmail.ch>
2022-10-10 10:58:46 +1300

    Update popper config

--- conf/60-popper.el
+++ conf/60-popper.el
@@ -7,6 +7,8 @@
   (setq popper-reference-buffers
         '("\\*Messages\\*"
           "Output\\*$"
+          "\\*Async Shell Command\\*"
           help-mode
           compilation-mode))
-  (popper-mode +1))
+  (popper-mode +1)
+  (popper-echo-mode +1))                ; For echo area hints
