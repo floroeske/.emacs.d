840891d6c224ac10e228cf683da02b4b77c81a07
Flo Roeske <floroeske@protonmail.ch>
2022-10-10 10:59:05 +1300

    Update c-style

--- conf/50-c-style.el
+++ conf/50-c-style.el
@@ -1,5 +1,8 @@
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+;; (setq-default indent-tabs-mode nil)
+;; (setq-default tab-width 4)
 
 ;; (setq c-set-style "bsd")
-(setq c-basic-offset 4)
+;; (setq c-basic-offset 4)
+
+(setq c-default-style "bsd"
+  c-basic-offset 4)
